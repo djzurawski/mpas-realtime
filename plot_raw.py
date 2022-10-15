480d4e55f6182e659b705dda46bf27c3a11964ac
Daniel Zurawski <djzurawski@gmail.com>
2022-10-15 14:57:30 -0600

    more raw plot features

--- plot_raw.py
+++ plot_raw.py
@@ -10,6 +10,9 @@ import matplotlib.colors as mcolors
 import numpy as np
 from scipy.interpolate import griddata
 from metpy.plots import USCOUNTIES
+import simplekml
+from dateutil.parser import isoparse
+from datetime import timezone
 
 RADIAN_TO_DEGREE = 180 / np.pi
 M_PER_S_TO_KT = 1.94384
@@ -81,24 +84,19 @@ def longtitude_360_to_180(lon):
     return ((lon + 360) % 180) - 180
 
 
-def grid_data(x, y, z):
+def grid_data(x, y, z, side_len=None):
     """Interpolates 3 1D arrays of xcoords, ycoords, values
     into a grid.
     Useful for plotting windbarbs"""
 
-    x_side = np.sort(np.unique(x))
-    y_side = np.sort(np.unique(y))
+    if not side_len:
+        side_len = int(np.sqrt(len(x)))
 
-    # If necessary reduce grid dims to save compute time
-    max_side_length = 500
-    if len(x) > max_side_length:
-        interval = int(len(x_side) / max_side_length)
-        x_side = x_side[::interval]
-        y_side = y_side[::interval]
+    x_pts = np.linspace(np.min(x), np.max(x), side_len)
+    y_pts = np.linspace(np.min(y), np.max(y), side_len)
 
-    grid_x, grid_y = np.meshgrid(x_side, y_side)
-
-    grid_z = griddata((x, y), z, (grid_x, grid_y))
+    grid_x, grid_y = np.meshgrid(x_pts, y_pts)
+    grid_z = griddata((x, y), z, (grid_x, grid_y), method="cubic")
     return grid_x, grid_y, grid_z
 
 
@@ -174,7 +172,7 @@ def add_rel_vorticity(fig, ax, lons, lats, rel_vort):
 
 
 def add_wind_barbs(
-    fig, ax, grid_lon, grid_lat, grid_u, grid_v, barb_length=5.5, barb_interval=0
+    fig, ax, grid_lon, grid_lat, grid_u, grid_v, barb_length=5.5, barb_interval=10
 ):
     step = barb_interval
     ax.barbs(
@@ -188,6 +186,30 @@ def add_wind_barbs(
     return fig, ax
 
 
+def ds_times(ds):
+    valid_date_str = ds.xtime.values[0].strip()
+    valid_dt = isoparse(valid_date_str).replace(tzinfo=timezone.utc)
+
+    initial_date_str = ds.initial_time.item().decode().strip()
+    initial_dt = isoparse(initial_date_str).replace(tzinfo=timezone.utc)
+
+    elapsed_secs = (valid_dt - initial_dt).total_seconds()
+    secs_per_hour = 3600
+    elapsed_hours = int(elapsed_secs // secs_per_hour)
+
+    return initial_dt, valid_dt, elapsed_hours
+
+
+def plot_title(init_dt, valid_dt, fhour, field_name, model_name="", field_units=""):
+
+    date_format = "%Y-%m-%dT%HZ"
+    init_str = init_dt.strftime(date_format)
+    valid_str = valid_dt.strftime(date_format)
+    fhour = str(fhour).zfill(2)
+
+    return f"{model_name}   Init: {init_str}    Valid: {valid_str}    {field_name} ({field_units})   Hour: {fhour}"
+
+
 def plot_500_vorticity(outfile_path, mesh_path):
     """outfile_path: Path of MPAS output file (history*, diagnostics*)
        mesh_path: Path of static/init mesh to provide cell lat/lons.
@@ -195,8 +217,10 @@ def plot_500_vorticity(outfile_path, mesh_path):
     Note: Some fields are on cell coordinates and some are on vertex coordinates
            hence the _cell and _vert suffix
     """
-    ds = xr.open_dataset("diag.2022-10-13_22.00.00.nc")
-    ds_cells = xr.open_dataset("colorado.grid.nc")
+    ds = xr.open_dataset("diag-small.nc")
+    ds_cells = xr.open_dataset("colorado-small.static.nc")
+
+    init_dt, valid_dt, fhour = ds_times(ds)
 
     lats_cell = ds_cells["latCell"] * RADIAN_TO_DEGREE
     lons_cell = ds_cells["lonCell"] * RADIAN_TO_DEGREE
@@ -229,4 +253,58 @@ def plot_500_vorticity(outfile_path, mesh_path):
 
     ax.set_xlim((np.min(lons_vert), np.max(lons_vert)))
     ax.set_ylim((np.min(lats_vert), np.max(lats_vert)))
+
+    title = plot_title(init_dt, valid_dt, fhour, "Rel Vort", "Dan MPAS", "10^5 s^-1")
+    ax.set_title(title)
     fig.show()
+
+
+def interp_terrain(grid_path):
+    "Plots terrain using two methods"
+    ds_cells = xr.open_dataset(grid_path)
+    lats_cell = ds_cells["latCell"] * RADIAN_TO_DEGREE
+    lons_cell = ds_cells["lonCell"] * RADIAN_TO_DEGREE
+    lons_cell = longtitude_360_to_180(lons_cell)
+    ter = ds_cells["ter"]
+    ter_ft = ter * 3.281
+
+    fig1, ax1 = basemap()
+    fig2, ax2 = basemap()
+
+    grid_x, grid_y, grid_h = grid_data(
+        lons_cell,
+        lats_cell,
+        ter_ft,
+    )
+    ax2.tripcolor(lons_cell, lats_cell, ter_ft, transform=crs.PlateCarree())
+    ax1.contourf(grid_x, grid_y, grid_h, levels=500, transform=crs.PlateCarree())
+    plt.show()
+
+
+def make_mesh(grid_path):
+    """Creates kml linestrings from mesh.
+    Caution: Will take a long time to make.
+             Will take long time to draw on map
+             Only use on regional meshes"""
+    ds_cells = xr.open_dataset(grid_path)
+    verts_on_edge = ds_cells["verticesOnEdge"]
+    lats_vert = ds_cells["latVertex"] * RADIAN_TO_DEGREE
+    lons_vert = ds_cells["lonVertex"] * RADIAN_TO_DEGREE
+    lons_vert = longtitude_360_to_180(lons_vert)
+
+    kml = simplekml.Kml()
+    for i, vert_idx in enumerate(verts_on_edge):
+        if i % 1000 == 0:
+            print(i, i / len(verts_on_edge))
+
+        x1 = float(lons_vert[vert_idx[0] - 1])
+        x2 = float(lons_vert[vert_idx[1] - 1])
+
+        y1 = float(lats_vert[vert_idx[0] - 1])
+        y2 = float(lats_vert[vert_idx[1] - 1])
+
+        ls = kml.newlinestring()
+        ls.coords = [(x1, y1), (x2, y2)]
+        ls.style.linestyle.width = 1
+        ls.style.linestyle.color = simplekml.Color.blue
+    kml.save("mpas_mesh.kml")
