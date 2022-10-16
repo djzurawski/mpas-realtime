f4e473c0f85144156c1631164a43e8a1d53fdbfe
Daniel Zurawski <djzurawski@gmail.com>
2022-10-15 18:47:03 -0600

    Add precip and 700mb relative humidity

--- plot_raw.py
+++ plot_raw.py
@@ -16,6 +16,7 @@ from datetime import timezone
 
 RADIAN_TO_DEGREE = 180 / np.pi
 M_PER_S_TO_KT = 1.94384
+MM_TO_IN = 0.03937008
 
 VORT_CMAP = (
     np.array(
@@ -78,6 +79,77 @@ VORT_LEVELS = [
     85,
 ]
 
+PRECIP_CLEVS = [
+    0.01,
+    0.025,
+    0.05,
+    0.075,
+    0.1,
+    0.15,
+    0.2,
+    0.25,
+    0.3,
+    0.4,
+    0.5,
+    0.6,
+    0.7,
+    0.8,
+    0.9,
+    1,
+    1.2,
+    1.4,
+    1.6,
+    1.8,
+    2.0,
+    2.5,
+    3,
+    3.5,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+]
+
+PRECIP_CMAP_DATA = (
+    np.array(
+        [
+            (190, 190, 190),
+            (170, 165, 165),
+            (130, 130, 130),
+            (110, 110, 110),
+            (180, 250, 170),
+            (150, 245, 140),
+            (120, 245, 115),
+            (80, 240, 80),
+            (30, 180, 30),
+            (15, 160, 15),
+            (20, 100, 210),
+            (40, 130, 240),
+            (80, 165, 245),
+            (150, 210, 230),
+            (180, 240, 250),
+            (255, 250, 170),
+            (255, 232, 120),
+            (255, 192, 60),
+            (253, 159, 0),
+            (255, 96, 0),
+            (253, 49, 0),
+            (225, 20, 20),
+            (191, 0, 0),
+            (165, 0, 0),
+            (135, 0, 0),
+            (99, 59, 59),
+            (139, 99, 89),
+            (179, 139, 129),
+            (199, 159, 149),
+            (240, 240, 210),
+        ]
+    )
+    / 255.0
+)
+
 
 def longtitude_360_to_180(lon):
     "Converts 0:360 longitude to -180:180"
@@ -134,16 +206,13 @@ def basemap():
     return fig, ax
 
 
-def add_500hPa_hgt(fig, ax, lons, lats, hgt_500):
+def add_geopotential_hgt(fig, ax, lons, lats, hgt, levels):
 
-    hgt_levels = np.arange(492, 594, 3)
-
-    hgt_500_dm = hgt_500 / 10
     contours = ax.tricontour(
         lons,
         lats,
-        hgt_500_dm,
-        levels=hgt_levels,
+        hgt,
+        levels=levels,
         colors="black",
         transform=crs.PlateCarree(),
     )
@@ -157,6 +226,25 @@ def add_rel_vorticity(fig, ax, lons, lats, rel_vort):
     norm = mcolors.BoundaryNorm(VORT_LEVELS, cmap.N)
 
     vort_contours = ax.tricontourf(
+        lons,
+        lats,
+        rel_vort,
+        VORT_LEVELS,
+        levels=VORT_LEVELS,
+        norm=norm,
+        cmap=cmap,
+        transform=crs.PlateCarree(),
+    )
+    plt.colorbar(vort_contours, ax=ax, orientation="vertical", pad=0.05)
+
+    return fig, ax
+
+
+def add_rel_vorticity_grid(fig, ax, lons, lats, rel_vort):
+    cmap = mcolors.ListedColormap(VORT_CMAP)
+    norm = mcolors.BoundaryNorm(VORT_LEVELS, cmap.N)
+
+    vort_contours = ax.contourf(
         lons,
         lats,
         rel_vort,
@@ -186,6 +274,52 @@ def add_wind_barbs(
     return fig, ax
 
 
+def add_relative_humidity(fig, ax, lons, lats, rh):
+    print(np.nanmin(rh), np.nanmax(rh))
+
+    rh_levels = [
+        0,
+        1,
+        2,
+        3,
+        5,
+        10,
+        15,
+        20,
+        25,
+        30,
+        40,
+        50,
+        60,
+        65,
+        70,
+        75,
+        80,
+        85,
+        90,
+        95,
+        99,
+        100,
+    ]
+
+    cmap = get_cmap("BrBG")
+    norm = mcolors.BoundaryNorm(rh_levels, cmap.N)
+
+    rh_contours = ax.tricontourf(
+        lons,
+        lats,
+        rh,
+        levels=rh_levels,
+        cmap=cmap,
+        norm=norm,
+        transform=crs.PlateCarree(),
+        transform_first=True,
+    )
+
+    fig.colorbar(rh_contours, ax=ax, orientation="vertical", pad=0.05)
+    return fig, ax
+
+
 def ds_times(ds):
     valid_date_str = ds.xtime.values[0].strip()
     valid_dt = isoparse(valid_date_str).replace(tzinfo=timezone.utc)
@@ -210,6 +344,53 @@ def plot_title(init_dt, valid_dt, fhour, field_name, model_name="", field_units=
     return f"{model_name}   Init: {init_str}    Valid: {valid_str}    {field_name} ({field_units})   Hour: {fhour}"
 
 
+def plot_rain(outfile_path, mesh_path):
+
+    """outfile_path: Path of MPAS output file (history*, diagnostics*)
+       mesh_path: Path of static/init mesh to provide cell lat/lons.
+
+    Note: Some fields are on cell coordinates and some are on vertex coordinates
+           hence the _cell and _vert suffix
+    """
+    # ds = xr.open_dataset("diag.2022-10-16_01.00.00.nc")
+    # ds = xr.open_dataset("diag.2022-10-16_14.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-16_21.00.00.nc")
+
+    ds_cells = xr.open_dataset("colorado-small.static.nc")
+
+    init_dt, valid_dt, fhour = ds_times(ds)
+
+    lats_cell = ds_cells["latCell"] * RADIAN_TO_DEGREE
+    lons_cell = ds_cells["lonCell"] * RADIAN_TO_DEGREE
+    lons_cell = longtitude_360_to_180(lons_cell)
+
+    lats_vert = ds_cells["latVertex"] * RADIAN_TO_DEGREE
+    lons_vert = ds_cells["lonVertex"] * RADIAN_TO_DEGREE
+    lons_vert = longtitude_360_to_180(lons_vert)
+
+    rain_in = ds["rainnc"][0] * MM_TO_IN
+
+    fig, ax = basemap()
+
+    cmap = mcolors.ListedColormap(PRECIP_CMAP_DATA)
+    norm = mcolors.BoundaryNorm(PRECIP_CLEVS, cmap.N)
+
+    rain_contours = ax.tricontourf(
+        lons_cell,
+        lats_cell,
+        rain_in,
+        PRECIP_CLEVS,
+        levels=PRECIP_CLEVS,
+        cmap=cmap,
+        norm=norm,
+        tranform=crs.PlateCarree(),
+    )
+    fig.colorbar(rain_contours, ax=ax, orientation="vertical", pad=0.05)
+    title = plot_title(init_dt, valid_dt, fhour, "Rel Vort", "Dan MPAS", "10^5 s^-1")
+    ax.set_title(title)
+    fig.show()
+
+
 def plot_500_vorticity(outfile_path, mesh_path):
     """outfile_path: Path of MPAS output file (history*, diagnostics*)
        mesh_path: Path of static/init mesh to provide cell lat/lons.
@@ -217,7 +398,12 @@ def plot_500_vorticity(outfile_path, mesh_path):
     Note: Some fields are on cell coordinates and some are on vertex coordinates
            hence the _cell and _vert suffix
     """
-    ds = xr.open_dataset("diag-small.nc")
+
+    # ds = xr.open_dataset("diag.2022-10-16_01.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-15_18.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-16_14.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-16_21.00.00.nc")
+
     ds_cells = xr.open_dataset("colorado-small.static.nc")
 
     init_dt, valid_dt, fhour = ds_times(ds)
@@ -231,17 +417,23 @@ def plot_500_vorticity(outfile_path, mesh_path):
     lons_vert = longtitude_360_to_180(lons_vert)
 
     hgt_500_cell = ds["height_500hPa"][0]
+    hgt_500_cell_dm = hgt_500_cell / 10
+    hgt_levels = np.arange(492, 594, 3)
     u_500_cell = ds["uzonal_500hPa"][0] * M_PER_S_TO_KT
     v_500_cell = ds["umeridional_500hPa"][0] * M_PER_S_TO_KT
     vort_500_vert = ds["vorticity_500hPa"][0]
     vort_500_vert_scaled = vort_500_vert * 10**5
 
     grid_500_x, grid_500_y, grid_500_u = grid_data(lons_cell, lats_cell, u_500_cell)
+    # grid_500_xvort, grid_500_yvort, grid_500_vert_scaled = grid_data(lons_vert, lats_vert, vort_500_vert_scaled)
     _, _, grid_500_v = grid_data(lons_cell, lats_cell, v_500_cell)
 
     fig, ax = basemap()
-    fig, ax = add_500hPa_hgt(fig, ax, lons_cell, lats_cell, hgt_500_cell)
+    fig, ax = add_geopotential_hgt(
+        fig, ax, lons_cell, lats_cell, hgt_500_cell_dm, hgt_levels
+    )
     fig, ax = add_rel_vorticity(fig, ax, lons_vert, lats_vert, vort_500_vert_scaled)
+    # fig, ax = add_rel_vorticity_grid(fig, ax, grid_500_xvort, grid_500_yvort, grid_500_vert_scaled)
     fig, ax = add_wind_barbs(
         fig,
         ax,
@@ -255,6 +447,62 @@ def plot_500_vorticity(outfile_path, mesh_path):
     ax.set_ylim((np.min(lats_vert), np.max(lats_vert)))
 
     title = plot_title(init_dt, valid_dt, fhour, "Rel Vort", "Dan MPAS", "10^5 s^-1")
+    ax.set_title(title)
+    fig.show()
+
+
+def plot_700_rh(outfile_path, mesh_path):
+    """outfile_path: Path of MPAS output file (history*, diagnostics*)
+       mesh_path: Path of static/init mesh to provide cell lat/lons.
+
+    Note: Some fields are on cell coordinates and some are on vertex coordinates
+           hence the _cell and _vert suffix
+    """
+
+    # ds = xr.open_dataset("diag.2022-10-16_01.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-15_18.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-16_14.00.00.nc")
+    ds = xr.open_dataset("diag.2022-10-16_21.00.00.nc")
+
+    ds_cells = xr.open_dataset("colorado-small.static.nc")
+
+    init_dt, valid_dt, fhour = ds_times(ds)
+
+    lats_cell = ds_cells["latCell"] * RADIAN_TO_DEGREE
+    lons_cell = ds_cells["lonCell"] * RADIAN_TO_DEGREE
+    lons_cell = longtitude_360_to_180(lons_cell)
+
+    rh_700 = ds["relhum_700hPa"][0]
+    hgt_700_cell = ds["height_700hPa"][0]
+    hgt_700_cell_dm = hgt_700_cell / 10
+    hgt_700_levels = np.arange(180, 420, 3)
+
+    u_700_cell = ds["uzonal_700hPa"][0] * M_PER_S_TO_KT
+    v_700_cell = ds["umeridional_700hPa"][0] * M_PER_S_TO_KT
+
+    grid_700_x, grid_700_y, grid_700_u = grid_data(lons_cell, lats_cell, u_700_cell)
+    _, _, grid_700_v = grid_data(lons_cell, lats_cell, v_700_cell)
+
+    fig, ax = basemap()
+    fig, ax = add_geopotential_hgt(
+        fig,
+        ax,
+        lons_cell,
+        lats_cell,
+        hgt_700_cell_dm,
+        hgt_700_levels,
+    )
+    fig, ax = add_relative_humidity(fig, ax, lons_cell, lats_cell, rh_700)
+    fig, ax = add_wind_barbs(
+        fig,
+        ax,
+        grid_700_x,
+        grid_700_y,
+        grid_700_u,
+        grid_700_v,
+    )
+
+    title = plot_title(init_dt, valid_dt, fhour, "Relative Humidity", "Dan MPAS", "%")
     ax.set_title(title)
     fig.show()
 
