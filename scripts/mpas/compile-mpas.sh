ce80e306310e09f0278bfe47e735dc0d8ea29d17
Daniel Zurawski <djzurawski@gmail.com>
2022-10-18 19:59:01 -0600

    install/devops update

--- scripts/mpas/compile-mpas.sh
+++ scripts/mpas/compile-mpas.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-
 LIB_PREFIX="$(pwd)/library"
-PROGRAM_DIR="$(pwd)/program"
 
-export PIO=$LIB_PREFIX
-export NETCDF=$LIB_PREFIX
-export PNETCDF=$LIB_PREFIX
-export LD_LIBRARY_PATH=$LIB_PREFIX/lib:$LD_LIBRARY_PATH
-export PATH=$LIB_PREFIX/bin:$PATH
-export MPAS_EXTERNAL_LIBS="-L$LIB_PREFIX/lib -lhdf5_hl -lhdf5 -ldl -lz"
-export MPAS_EXTERNAL_INCLUDES="-I$LIB_PREFIX/include"
+CPU_CORES=$(nproc)
 
-cd $PROGRAM_DIR
-wget -c https://github.com/MPAS-Dev/MPAS-Model/archive/refs/tags/v7.3.tar.gz
-tar -xzvf v7.3.tar.gz
-cd MPAS-Model-7.3
+cd $ROOT_DIR
+git clone https://github.com/MPAS-Dev/MPAS-Model
+cd MPAS-Model
 make clean CORE=init_atmosphere USE_PIO2=true
-make -j6 gfortran CORE=init_atmosphere USE_PIO2=true PRECISION=single
+make -j $CPU_CORES gfortran CORE=init_atmosphere USE_PIO2=true PRECISION=single
 make clean CORE=atmosphere USE_PIO2=true
-make -j6 gfortran CORE=atmosphere USE_PIO2=true PRECISION=single
+make -j $CPU_CORES gfortran CORE=atmosphere USE_PIO2=true PRECISION=single
+./build-tables
