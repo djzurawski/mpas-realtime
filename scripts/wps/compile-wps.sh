9738e8c66fa9f058079d6da3242e51631625b48f
Daniel Zurawski <djzurawski@gmail.com>
2022-10-20 21:11:44 -0600

    Fix compile wps script

--- scripts/wps/compile-wps.sh
+++ scripts/wps/compile-wps.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
-echo $LD_LIBRARY_PATH
-
-cd program/
-#wget -c https://github.com/wrf-model/WPS/archive/refs/tags/v4.4.tar.gz
-#tar -xzvf v4.4.tar.gz
-#rm v4.4.tar.gz
+cd $TOOLS_DIR
+wget -c https://github.com/wrf-model/WPS/archive/refs/tags/v4.4.tar.gz
+tar -xzvf v4.4.tar.gz
+rm v4.4.tar.gz
 cd WPS-4.4
 ./configure --build-grib2-libs --nowrf
 ./compile
